372565b47f648dbbfef1e31a4a0f2ac8c92efdb7
Akhya04 <159602038+Akhya04@users.noreply.github.com>
2026-03-28 10:47:15 +0530

    Delete 21-03-26/com/cg directory

--- 21-03-26/com/cg/exception/DepartmentNameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cg.exception;
-
-public class DepartmentNameNotFoundException extends RuntimeException {
-    public DepartmentNameNotFoundException(String message) {
-        super(message);
-    }
-}
--- 21-03-26/com/cg/exception/MobileNumberDoesNotExistsForEmployeeException.java
+++
@@ -1,7 +0,0 @@
-package com.cg.exception;
-
-public class MobileNumberDoesNotExistsForEmployeeException extends RuntimeException {
-    public MobileNumberDoesNotExistsForEmployeeException(String message) {
-        super(message);
-    }
-}
